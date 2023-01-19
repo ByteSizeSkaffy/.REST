1b084db5cbdf0755d26ad1bb5070fe3dd7ce1c87
ByteSizeSkaffy <0978218@hr.nl>
2023-01-19 16:12:42 +0100

    1-0

---
+++ Backend/Endpoints.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+using Backend;
+
+namespace Backend{
+    public class Person{
+        public int Id;
+        public string Name {get; set;}
+        public int Age {get;set;}
+    }
+
+    public class FriendsWith{
+        public int FromId {get; set;}
+        public int ToId {get; set;}
+    }
+    public class inMemoryDb{
+        public List<Person> Person {get; set;} 
+        public List<FriendsWith> FriendShip {get; set;}
+
+        public void inMemoryDatabase(){
+            Person = new List<Person>();
+            FriendShip = new List<FriendsWith>();
+        }
+    }
+    public class EndPointManager{
+        private WebApplication App {get;set;}
+        private inMemoryDb Db {get;set;}
+        public EndPointManager(WebApplication app){
+            App = app;
+            Db = new inMemoryDb();
+            App.MapGet("/helloworld", ()=> Results.Ok("hello world"));
+            App.MapPost("/person", AddPerson);
+        }
+        private IResult AddPerson(Person person){
+            var existingPerson = Db.Person.FirstOrDefault(p => p.Id == person.Id);
+            if(existingPerson==null){
+                return Results.BadRequest($"person with id: {person.Id} already exists");
+            }
+            Db.Person.Add(person);
+            return Results.Ok("succeeded");
+             
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Backend;
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.Urls.Add("http://localhost:5000");
+
+var EndpointManager = new EndPointManager(app);
+
+app.MapGet("/hello123/{who1}/{who2}/{who3}", (string who1, string who2, string who3) => $"Hello World from {who1}, {who2}, and {who3}!!!");
+app.MapPost("/person", (Person who) => $"The following person was posted: {who}");
+
+app.Run();
+
+
+record Person(string Name, int Age);
